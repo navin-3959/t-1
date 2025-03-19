17a90c304b44ac51cc2a36dcfc8ed3811c908dd2
Navin kumar <navinkumar3959@gmail.com>
2025-03-19 22:06:05 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeroSection from "../components/Herosection";
 import ServiceCards from "../components/Servicecards";
-import PricingTable from "../components/PricingTable";
+import PricingTable from "../components/Pricingtable";
 import Contact from "../components/Contact";
 import UserList from '../components/Userlist';
 const Home = ({users}) => {
